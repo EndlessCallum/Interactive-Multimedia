89265bed0507c2b15443df576f8b8c0d1ab01194
EndlessCallum <71830131+EndlessCallum@users.noreply.github.com>
2020-11-24 14:35:51 +0000

    Player control + added vehicle

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public float speed;
+    public float turnspeed;
+    private float horizontalInput;
+    private float forwardInput;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * turnspeed * horizontalInput * Time.deltaTime);
+    }
+}
